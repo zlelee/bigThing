aa977a49918875c32e3d64c0c252101a4f0735cb
lele <562282501@qq.com>
2021-01-13 11:39:28 +0800

    初步验证用户名及密码

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,6 +1,24 @@
 const express = require('express')
+const db = require('../db/index')
 exports.reguser = (req, res) => {
-  res.send('reguser ok')
+  const userinfo = req.body
+  if (!userinfo.username || !userinfo.password) {
+    return res.send({
+      status: 1,
+      message: '用户名或密码不能为空'
+    })
+  }
+  const sql = 'select * from ev_users where username=?'
+  db.query(sql, [userinfo.username], (err, results) => {
+    if (err) return res.send({ status: 1, message: err.message })
+    if (results.length > 0) {
+      res.send({
+        status: 1,
+        message: '用户名被其他人占用,请更换用户名'
+      })
+    }
+    res.send('可以注册')
+  })
 }
 exports.login = (req, res) => {
   res.send('login ok')
