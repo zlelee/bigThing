a214aebb7c27e02d0e62b3d784fa15b21c45af41
lele <562282501@qq.com>
2021-01-13 14:27:22 +0800

    注册功能的完善

--- router_handler/user.js
+++ router_handler/user.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bcrypt = require('bcryptjs')
 const db = require('../db/index')
 exports.reguser = (req, res) => {
   const userinfo = req.body
+  //检测是否为空
   if (!userinfo.username || !userinfo.password) {
     return res.send({
       status: 1,
@@ -11,13 +13,28 @@ exports.reguser = (req, res) => {
   const sql = 'select * from ev_users where username=?'
   db.query(sql, [userinfo.username], (err, results) => {
     if (err) return res.send({ status: 1, message: err.message })
+    //检测用户名是否被占用
     if (results.length > 0) {
       res.send({
         status: 1,
         message: '用户名被其他人占用,请更换用户名'
       })
     }
-    res.send('可以注册')
+    //给密码加密, 10代表随机盐的长度
+    userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+    const sql = 'insert into ev_users set ?'
+    db.query(
+      sql,
+      {
+        username: userinfo.username,
+        password: userinfo.password
+      },
+      (err, results) => {
+        if (err) return res.send({ status: 1, message: err.message })
+        if (results.affectedRows !== 1) return res.send({ status: 1, message: '注册失败,请再试一次' })
+        res.send({ status: 0, message: '注册成功' })
+      }
+    )
   })
 }
 exports.login = (req, res) => {
