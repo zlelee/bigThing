92799253315b1cc1fbd3a1dfe405d965f10758b1
lele <562282501@qq.com>
2021-01-14 18:40:27 +0800

    重置密码

--- router/userinfo.js
+++ router/userinfo.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const expressJoi = require('@escook/express-joi')
 // 导入需要的验证规则对象
-const { update_userinfo_schema } = require('../schema/user')
+const { update_userinfo_schema, update_password_schema } = require('../schema/user')
 
 // const db = require('../db/index')
 // 导入用户信息的处理函数模块
@@ -12,5 +12,6 @@ const userinfo_handler = require('../router_handler/userinfo')
 router.get('/userinfo', userinfo_handler.getUserInfo)
 // 更新用户的基本信息
 router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo)
-
+//重置密码的路由
+router.post('/updatepwd', expressJoi(update_password_schema), userinfo_handler.updatePassword)
 module.exports = router
--- router_handler/userinfo.js
+++ router_handler/userinfo.js
@@ -33,3 +33,42 @@ module.exports.updateUserInfo = (req, res) => {
     return res.cc('修改用户基本信息成功！', 0)
   })
 }
+module.exports.updatePassword = (req, res) => {
+  // 定义根据 id 查询用户数据的 SQL 语句
+  const sql = `select * from ev_users where id=?`
+
+  // 执行 SQL 语句查询用户是否存在
+  db.query(sql, req.user.id, (err, results) => {
+    // 执行 SQL 语句失败
+    if (err) return res.cc(err)
+
+    // 检查指定 id 的用户是否存在
+    if (results.length !== 1) return res.cc('用户不存在！')
+
+    // 在头部区域导入 bcryptjs 后，
+    // 即可使用 bcrypt.compareSync(提交的密码，数据库中的密码) 方法验证密码是否正确
+    // compareSync() 函数的返回值为布尔值，true 表示密码正确，false 表示密码错误
+    const bcrypt = require('bcryptjs')
+
+    // 判断提交的旧密码是否正确
+    const compareResult = bcrypt.compareSync(req.body.oldPwd, results[0].password)
+    if (!compareResult) return res.cc('原密码错误！')
+    // 定义更新用户密码的 SQL 语句
+    const sql = `update ev_users set password=? where id=?`
+
+    // 对新密码进行 bcrypt 加密处理
+    const newPwd = bcrypt.hashSync(req.body.newPwd, 10)
+
+    // 执行 SQL 语句，根据 id 更新用户的密码
+    db.query(sql, [newPwd, req.user.id], (err, results) => {
+      // SQL 语句执行失败
+      if (err) return res.cc(err)
+
+      // SQL 语句执行成功，但是影响行数不等于 1
+      if (results.affectedRows !== 1) return res.cc('更新密码失败！')
+
+      // 更新密码成功
+      res.cc('更新密码成功！', 0)
+    })
+  })
+}
